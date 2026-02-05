c27b51f29937af02f5fcf7f42223d6ba4f3c4442
sweetlypriya <sweetlypriya9@gmail.com>
2026-02-05 22:12:43 +0530

    Scopes and Loops

---
+++ JavaScript/StatementsAndLoops/Loops.js
@@ -0,0 +1,33 @@
+//MODERN LOOPS
+
+//for in loop 
+//it directly prints index value
+//based on index we print values
+
+let tools=["Playwright","Selenium","Cypress","Pupeteer"]
+
+for(let index in tools){
+    console.log(index)
+    console.log(tools[index])
+
+    if(tools[index]==="Cypress"){
+        console.log("Successfully logged in "+tools[index])
+    }
+}
+
+//for of loop
+//it directly prints the value
+//doe not print index
+
+for(let value of tools){
+
+    console.log(value)
+
+}
+
+//TRADITIONAL LOOPS
+
+//for loop
+for(let i=0;i<tools.length;i++){
+    console.log("for loop: "+tools[i])
+}
