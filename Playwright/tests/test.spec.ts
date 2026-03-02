226d5e5fcac7780bcd5bff92787558c7b94d0fa1
sweetlypriya <sweetlypriya9@gmail.com>
2026-03-02 17:56:41 +0530

    Handling elements 1

--- Playwright/tests/test.spec.ts
+++ Playwright/tests/test.spec.ts
@@ -1,5 +1,27 @@
-import{test,expect} from '@playwright/test'
+import {test,expect} from '@playwright/test'
 
-test('login validation',async({page})=>{
-    await page.goto('https://automationexercise.com/')
+test.describe('Basic Element Handling and Assertions',()=>{
+    
+    test('SignUp by handling goto,fill,type,click,clear,hover,focus,dblclick,expect-toHaveURL,toContain',async({page})=>{
+        
+        await page.goto("https://automationexercise.com/")
+        await page.getByRole('link', { name: ' Signup / Login' }).click()
+        await expect(page).toHaveURL('https://automationexercise.com/login')
+        await page.getByPlaceholder('Name').fill('spriya')
+        await page.locator('.signup-form').filter({hasText:'New User Signup!'}).locator('form').getByPlaceholder('Email Address').type('sspprriyaa@gmail.com')
+        await page.locator('.signup-form').filter({hasText:'New User Signup!'}).locator('form').getByPlaceholder('Email Address').clear()
+        await page.locator('.signup-form').filter({hasText:'New User Signup!'}).locator('form').getByPlaceholder('Email Address').type('sspprriyaa@gmail.com')
+        await page.getByRole('button',{name:'Signup'}).click()
+        //await expect(page.url).toContain('signup')
+        await page.getByRole('link',{name:'Contact us'}).hover()
+        await page.getByRole('link',{name:'Products'}).focus()
+        await page.getByRole('checkbox',{name:'Sign up for our newsletter!'}).dblclick()
+        await page.locator('#id_gender2').check()
+
+
+    })
+
+   
+
+
 })
