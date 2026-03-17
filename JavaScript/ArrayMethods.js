89a4faace5c3731069135dbfe4b292b3151b1cd0
sweetlypriya <sweetlypriya9@gmail.com>
2026-03-17 19:32:56 +0530

    updates

--- JavaScript/ArrayMethods.js
+++ JavaScript/ArrayMethods.js
@@ -1,14 +1,16 @@
 //1)array.push() => add the element at the end of the array
+//it modifies in the original array
 console.log("=======PUSH=======")
 
 const toolsAutomation=["Selenium","Playwright","Tosca","Cypress"]
 console.log("Before Push: "+toolsAutomation)
 toolsAutomation.push("Pupeteer","QTP")
-console.log("After Push: "+toolsAutomation)
+console.log("After Push: "+toolsAutomation) 
 toolsAutomation.push("Selenium")
 console.log(toolsAutomation)
 
 //2)array.pop() => removes last element from the array
+//it modifies in the original array
 console.log("======POP========")
 
 toolsAutomation.pop()
@@ -18,6 +20,7 @@ toolsAutomation.pop()
 console.log(toolsAutomation)
 
 //3)array.shift() => removes the first element from the array
+//it modifies in the original array
 console.log("=========SHIFT=========")
 
 toolsAutomation.shift()
@@ -26,12 +29,14 @@ toolsAutomation.shift()
 console.log(toolsAutomation)
 
 //4)array.unshift() => adds the element at the beginning of the array
+//it modifies in the original array
 console.log("=========UNSHIFT=======")
 
 toolsAutomation.unshift("PlayWright","Selenium")
 console.log(toolsAutomation)
 
 //5)array.includes() => check whether the element is present in the array or not
+//returns boolean value true or false
 console.log("========INCLUDES========")
 
 console.log(toolsAutomation.includes("Selenium"))
@@ -46,9 +51,10 @@ if(toolsAutomation.includes("QTP")){
 console.log("=========INDEX========")
 
 console.log("Index of Tosca: "+toolsAutomation.indexOf("Tosca"))
-console.log(toolsAutomation.indexOf("QTP"))
+console.log(toolsAutomation.indexOf("QTP")) //it returns index as -1 if the value is not present in array
 
 //7)array.concat() => it is used to merge two or more arrays
+//it doesnt modifies in the original array instead it stores in new array
 console.log("==========CONCAT=======")
 
 const tools=["Selenium","Playwright"]
@@ -57,6 +63,7 @@ const joinedTools=tools.concat(programmingLang)
 console.log(joinedTools)
 
 //8)array.reverse() => it is used to reverse the order of the elements in the array
+//it modifies in the original array
 console.log("=========REVERSE========")
 
 toolsAutomation.reverse()
@@ -68,17 +75,18 @@ console.log(toolsAutomation)
 console.log("===========FLAT========")
 
 let nestedItems=[1,2,3,4,['A','B','C'],['a','b','c',[6,[7,8],9]]]
-console.log(nestedItems.flat(1))
-console.log(nestedItems.flat(2))
-console.log(nestedItems.flat(Infinity))
+console.log(nestedItems.flat(1)) //flatten 1 nested array
+console.log(nestedItems.flat(2)) //flatten 2 nested array
+console.log(nestedItems.flat(Infinity)) //flatten all the nested array available
 
 //10)array.join() => join the elements of the array into a string
 console.log("=========JOIN========")
 
-console.log(toolsAutomation.join())
-console.log(toolsAutomation.join())
+console.log(toolsAutomation.join(",")) //it join array into string using "," eg: "PlayWright,Selenium,Tosca,Cypress,Pupeteer"
+console.log(toolsAutomation.join()) //by default it will join usin ","
+console.log(toolsAutomation.join("_")) //it join array into string using "_" eg: "PlayWright_Selenium_Tosca_Cypress_Pupeteer"
 
-//11)array.forEach() => execute a provided function once for each array element
+//11)array.forEach() => execute a provided function once for each array element 
 console.log("=======FOREACH=======")
 
 toolsAutomation.forEach((value,index,completeArray)=>{
